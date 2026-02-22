060c72879c58f187e41062e88d3d7da4b07db4ea
sainbunty000-lab <sainbunty000@gmail.com>
2026-02-22 16:20:14 +0530

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,9 +1,22 @@
-import os
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+from sqlalchemy.orm import sessionmaker, declarative_base
+from core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+# Dependency for FastAPI routes
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
