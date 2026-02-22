6b1f47137a68163caf195c270293300c901ec6bc
sainbunty000-lab <sainbunty000@gmail.com>
2026-02-22 16:23:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.wc_router import router as wc_router
+from core.database import engine
+from models.cam import CamReport
+from core.database import Base
 
 app = FastAPI(
     title="Credit Eligibility Backend",
     version="3.0.0"
 )
+
+Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
