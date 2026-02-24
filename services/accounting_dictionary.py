67f8f7a9ff367f0e71be8247c78d41e4eff88a44
sainbunty000-lab <sainbunty000@gmail.com>
2026-02-24 16:34:33 +0530

    Create accounting_dictionary.py

---
+++ services/accounting_dictionary.py
@@ -0,0 +1,39 @@
+ACCOUNTING_KEYWORDS = {
+    "current_assets": [
+        "current assets",
+        "total current assets"
+    ],
+    "current_liabilities": [
+        "current liabilities",
+        "total current liabilities"
+    ],
+    "inventory": [
+        "inventory",
+        "stock in trade",
+        "closing stock"
+    ],
+    "receivables": [
+        "trade receivables",
+        "sundry debtors",
+        "debtors"
+    ],
+    "payables": [
+        "trade payables",
+        "creditors"
+    ],
+    "sales": [
+        "revenue",
+        "sales",
+        "turnover",
+        "net sales"
+    ],
+    "cogs": [
+        "cost of goods sold",
+        "cogs",
+        "cost of sales"
+    ],
+    "bank_credit": [
+        "short term borrowings",
+        "working capital loan"
+    ]
+}
