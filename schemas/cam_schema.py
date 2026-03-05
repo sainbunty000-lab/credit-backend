34024c79e91990bccb8b38b3973a1da2b7ecb3b4
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-06 00:18:44 +0530

    Update cam_schema.py

--- schemas/cam_schema.py
+++ schemas/cam_schema.py
@@ -3,13 +3,28 @@ from typing import Optional, Dict
 
 
 class CAMCreate(BaseModel):
+
     customer_name: str
     wc_data: Optional[Dict] = {}
     agri_data: Optional[Dict] = {}
     banking_data: Optional[Dict] = {}
 
+    loan_amount: Optional[int] = 0
+    analyst_name: Optional[str] = "System"
+
 
 class CAMUpdate(BaseModel):
+
     wc_data: Optional[Dict] = None
     agri_data: Optional[Dict] = None
     banking_data: Optional[Dict] = None
+
+    loan_amount: Optional[int] = None
+    credit_grade: Optional[str] = None
+    remarks: Optional[str] = None
+
+
+class CAMSubmit(BaseModel):
+
+    credit_grade: str
+    remarks: Optional[str] = ""
