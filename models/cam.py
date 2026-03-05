50e0018c027205f7c1b102f7f2923bf126ce5f05
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-06 00:17:57 +0530

    Update cam.py

--- models/cam.py
+++ models/cam.py
@@ -2,13 +2,24 @@ from sqlalchemy import Column, Integer, String, JSON, DateTime
 from core.database import Base
 from datetime import datetime
 
+
 class CAMReport(Base):
     __tablename__ = "cam_reports"
 
     id = Column(Integer, primary_key=True, index=True)
+
     customer_name = Column(String, nullable=False)
-    wc_data = Column(JSON, default={})
-    agri_data = Column(JSON, default={})
-    banking_data = Column(JSON, default={})
+
+    wc_data = Column(JSON, default=dict)
+    agri_data = Column(JSON, default=dict)
+    banking_data = Column(JSON, default=dict)
+
     status = Column(String, default="Draft")
+
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    updated_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
