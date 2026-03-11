58edc5e5ac102353eb023f88acd6875bc34a0145
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-12 01:32:06 +0530

    Update accounting_dictionary.py

--- services/accounting_dictionary.py
+++ services/accounting_dictionary.py
@@ -13,14 +13,18 @@ ACCOUNTING_KEYWORDS = {
 "current assets",
 "total current assets",
 "current asset",
-"tca"
+"tca",
+"total ca",
+"current asset total"
 ],
 
 "current_liabilities":[
 "current liabilities",
 "total current liabilities",
 "current liability",
-"tcl"
+"tcl",
+"total cl",
+"current liability total"
 ],
 
 "inventory":[
@@ -28,35 +32,46 @@ ACCOUNTING_KEYWORDS = {
 "inventories",
 "stock",
 "stock in trade",
-"closing stock",
 "stock-in-trade",
+"closing stock",
+"opening stock",
 "finished goods",
-"raw material stock"
+"raw material",
+"raw material stock",
+"work in progress",
+"wip",
+"goods inventory"
 ],
 
 "receivables":[
 "trade receivables",
 "sundry debtors",
 "accounts receivable",
+"account receivable",
 "debtors",
 "receivables",
-"book debts"
+"book debts",
+"trade debtors"
 ],
 
 "payables":[
 "trade payables",
 "sundry creditors",
 "accounts payable",
+"account payable",
 "creditors",
-"payables"
+"payables",
+"trade creditors"
 ],
 
 "cash_bank":[
 "cash and bank",
+"cash and bank balance",
 "cash and cash equivalents",
 "cash balance",
 "bank balance",
-"cash in hand"
+"cash in hand",
+"cash at bank"
 ],
 
 "bank_credit":[
@@ -67,7 +82,9 @@ ACCOUNTING_KEYWORDS = {
 "working capital loan",
 "bank overdraft",
 "secured loan",
-"short term borrowings"
+"short term borrowings",
+"cc account",
+"od account"
 ],
 
 "other_current_assets":[
@@ -75,14 +92,18 @@ ACCOUNTING_KEYWORDS = {
 "short term loans and advances",
 "loans and advances",
 "advances",
-"other loans and advances"
+"other loans and advances",
+"advance to suppliers",
+"advance recoverable"
 ],
 
 "other_current_liabilities":[
 "other current liabilities",
 "short term provisions",
 "provisions",
-"other liabilities"
+"other liabilities",
+"statutory dues",
+"outstanding expenses"
 ],
 
 
@@ -100,13 +121,17 @@ ACCOUNTING_KEYWORDS = {
 "turnover",
 "sales turnover",
 "sale of products",
-"operating revenue"
+"operating revenue",
+"total revenue",
+"income from operations"
 ],
 
 "other_income":[
 "other income",
 "non operating income",
-"misc income"
+"misc income",
+"miscellaneous income",
+"other revenue"
 ],
 
 "cogs":[
@@ -114,14 +139,19 @@ ACCOUNTING_KEYWORDS = {
 "cost of sales",
 "cost of materials consumed",
 "direct cost",
-"purchase of stock in trade"
+"purchase of stock in trade",
+"cost of goods",
+"material consumed",
+"purchase cost"
 ],
 
 "operating_expenses":[
 "operating expenses",
 "administrative expenses",
 "selling expenses",
-"other expenses"
+"other expenses",
+"employee benefit expenses",
+"staff cost"
 ],
 
 "gross_profit":[
@@ -131,24 +161,28 @@ ACCOUNTING_KEYWORDS = {
 
 "ebitda":[
 "ebitda",
-"earnings before interest tax depreciation amortization"
+"earnings before interest tax depreciation amortization",
+"operating profit"
 ],
 
 "depreciation":[
 "depreciation",
-"depreciation expense"
+"depreciation expense",
+"depreciation and amortization"
 ],
 
 "interest_expense":[
 "interest",
 "finance cost",
 "finance charges",
-"interest expense"
+"interest expense",
+"interest on loans"
 ],
 
 "profit_before_tax":[
 "profit before tax",
-"pbt"
+"pbt",
+"profit before taxation"
 ],
 
 "net_profit":[
@@ -156,7 +190,8 @@ ACCOUNTING_KEYWORDS = {
 "profit after tax",
 "pat",
 "net income",
-"profit for the year"
+"profit for the year",
+"profit after taxation"
 ],
 
 
@@ -167,19 +202,22 @@ ACCOUNTING_KEYWORDS = {
 "salary_income":[
 "salary",
 "salary credit",
-"salary deposit"
+"salary deposit",
+"salary transfer"
 ],
 
 "upi_spend":[
 "upi",
 "upi payment",
-"upi transfer"
+"upi transfer",
+"upi txn"
 ],
 
 "loan_emi":[
 "emi",
 "loan installment",
-"installment"
+"installment",
+"loan emi"
 ]
 
 }
@@ -198,24 +236,29 @@ UNIT_SCALE_KEYWORDS = {
 "amounts in thousand",
 "amounts in thousands",
 "figures in thousand",
-"figures in thousands"
+"figures in thousands",
+"amount in thousand"
 ],
 
 "lakh":[
 "in lakh",
 "in lakhs",
 "amounts in lakhs",
-"figures in lakhs"
+"figures in lakhs",
+"amount in lakh"
 ],
 
 "million":[
 "in million",
-"in millions"
+"in millions",
+"amounts in million"
 ],
 
 "crore":[
 "in crore",
-"in crores"
+"in crores",
+"amounts in crores",
+"figures in crores"
 ]
 
 }
