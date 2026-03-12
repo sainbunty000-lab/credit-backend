cb1e54dbf387b4d0d46d9b30a2d8c29380f368aa
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-12 23:56:02 +0530

    Update and rename database.py to database.py

---
+++ nano core/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Temporary SQLite database
+DATABASE_URL = "sqlite:///./credit.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
