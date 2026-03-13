0f18025737f0e6fb45b8034246f66b07a9018317
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-13 15:52:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import time
 import logging
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
