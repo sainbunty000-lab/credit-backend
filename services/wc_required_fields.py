59dec375e9177c4223ac4a8ac578fc0178630ce6
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-14 12:53:19 +0530

    Update wc_required_fields.py

--- services/wc_required_fields.py
+++ services/wc_required_fields.py
@@ -1,1 +1,15 @@
-
+WC_REQUIRED_INPUT_FIELDS = [
+    "current_assets",
+    "current_liabilities",
+    "inventory",
+    "receivables",
+    "payables",
+    "cash_bank",
+    "bank_credit",
+    "annual_sales",
+    "other_income",
+    "operating_expenses",
+    "interest_expense",
+    "depreciation",
+    "tax",
+]
